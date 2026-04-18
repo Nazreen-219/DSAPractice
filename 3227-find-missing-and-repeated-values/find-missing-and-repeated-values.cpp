b5cf771792d7a0af1bbe9a9f81992c4b5da28e89
Nazreen Qureshi <qureshi992005@gmail.com>
2026-04-18 15:38:03 +0530

    Time: 15 ms (38.58%) | Memory: 31.7 MB (20.64%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
-        int n = grid.size();
         vector<int> ans;
         unordered_set<int> s;
-        int actualSum = 0;
-        int expSum =0;
-        int a , b;
-        
-        for(int i =0 ; i<n;i++){
-            for(int j =0; j<n; j++){
-                actualSum += grid[i][j];
-                if(s.find(grid[i][j]) != s.end()){
-                     a = grid[i][j];
+        int n=grid.size();
+        int a,b;
+        int expectedSum=0;
+        int actualSum =0;
+
+        for(int i=0; i<n;i++){
+            for(int j=0;j<n;j++){
+                actualSum+=grid[i][j];
+                if(s.find(grid[i][j])!=s.end()){    
+                    a=grid[i][j];
                     ans.push_back(a);
                 }
+
                 s.insert(grid[i][j]);
-            } 
-          
+            }
         }
-         expSum = (n*n )*(n*n +1) /2;
-         b = expSum + a - actualSum;
+        expectedSum = (n*n)*((n*n) +1)/2;
+        b = expectedSum+a-actualSum;
         ans.push_back(b);
-       
-        return ans;
+
+        return ans;     
     }
-    
 };

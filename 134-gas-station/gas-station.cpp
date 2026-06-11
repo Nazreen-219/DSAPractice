90b035ad27295b46207473ad33b31cdb5f8ef221
Nazreen Qureshi <qureshi992005@gmail.com>
2026-06-11 23:13:56 +0530

    Time: 5 ms (12.25%) | Memory: 112.5 MB (59.90%) - LeetSync

--- 134-gas-station/gas-station.cpp
+++ 134-gas-station/gas-station.cpp
@@ -3,23 +3,14 @@ public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
         int totgas = 0 , totcost = 0;
 
-        for(int val : gas){
-          totgas+= val;
-        }
-    
-        for(int val : cost){
-          totcost+= val;
-        }
-
-        if(totgas < totcost){
-            return -1;
-        }
-
+        
         // unique value always existe 
 
         int start = 0 ; int currGas = 0;
 
         for(int i =0 ; i < gas.size() ; i++){
+            totgas+= gas[i];
+            totcost+= cost[i];
             currGas+= (gas[i] - cost[i]);
 
             if(currGas <0){
@@ -27,6 +18,11 @@ public:
                 currGas = 0;
             }
         }
+
+        if(totgas < totcost){
+            return -1;
+        }
+
         return start;
     }
 };
